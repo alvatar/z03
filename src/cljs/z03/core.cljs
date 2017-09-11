4616be935156820e8af182e7740112411300d64f
alvatar <a@fourthbit.com>
2017-09-11 10:36:36 -0500

    CSS cleanup

--- src/cljs/z03/core.cljs
+++ src/cljs/z03/core.cljs
@@ -28,6 +28,29 @@
 (timbre/set-level! :debug)
 
 ;;
+;; State
+;;
+
+(def revisions
+  {:master {:description "[master] Branding v1"
+            :tags ["branding" "logo" "milestone"]
+            :files [{:file "logo.psd" :last-commit "[master] Branding v1"}
+                    {:file "file1.psd" :last-commit "Simplified logo; reduced number of colors"}
+                    {:file "file2.psd" :last-commit "[master] Branding v1"}
+                    {:file "file3.png" :last-commit "[master] Branding v1"}
+                    {:file "file4.jpg" :last-commit "[master] Branding v1"}
+                    {:file "file5.png" :last-commit "[master] Branding v1"}
+                    {:file "file6.jpg" :last-commit "[master] Branding v1"}]}
+   :head1 {:description "Simplified logo; reduced number of colors"
+           :tags ["logo" "tweak"]
+           :files [{:file "logo.psd" :last-commit "Simplified logo; reduced number of colors"}
+                   {:file "file1.psd" :last-commit "General structure for branding"}]}})
+
+(defonce ui-state
+  {:active-project (r/atom "Example")
+   :active-revision (r/atom (:master revisions))})
+
+;;
 ;; Event Handlers
 ;;
 
@@ -70,16 +93,17 @@
     {:font-family ["Oswald" "sans-serif"]
      :font-weight 200
      :overflow-x "hidden"
-     :line-height "1.2rem"}
-    [:h1 {}]]
+     :line-height "1.2rem"}]
    [:h1 :h2 {:font-family ["Bitter" "sans-serif"]
-             :font-weight 700
-             :line-height (u/rem 2.2)}]
+             :font-weight 700}]
+   [:h1 {:line-height (u/rem 2.8)}]
+   [:h2 {:line-height (u/rem 2.2)}]
    [:.fill-parent {:width "100%" :height "100%"}]
    [:.lfloat {:float "left"}]
    [:.rfloat {:float "right"}]
    [:.center {:text-align "center"}]
    [:.nomargin {:margin 0 :padding 0}]
+   [:.clickable {:cursor "pointer"}]
    ;; Colors
    [:.bg-aqua {:background-color "#7fdbff"}]
    ;; Animations
@@ -94,28 +118,20 @@
                                     :animation [[drawer-animation-hide "1.0s"]]})]])
    [:.file-menu]
    [:.file-item {:padding "0 2rem 1rem 2rem"}]
-   (let [files-top (u/px 280)
-         horizontal-container {:position "relative"
-                                :margin "0 auto"
-                                :width (u/px 900)}]
-     [[:.graph-section-container {:position "absolute"
-                                  :top 0 :left 0
-                                  :height files-top :width "100%"
-                                  :background-color "#ddd"}]
-      [:.graph-section-container-2 horizontal-container]
-      [:.files-section-container {:position "absolute"
-                                  :top files-top
-                                  :min-height (u/rem 40) :width "100%"
-                                  :background-color "#fff"}]
-      [:.files-section-container-2 horizontal-container]
-      [:.header-section-container {:position "absolute" :left 0 :top 0
-                                   :width "100%"
-                                   :font {:family ["Bitter" "sans-serif"]
-                                          :weight 400
-                                          :size (u/rem 0.7)}}]
-      [:.header-section-container-2 (merge horizontal-container
-                                           {:height (u/px 30)
-                                            :border {:style "solid" :width "0 0 1 0"}})]])
+   [:.container {:position "relative"
+                 :margin "0 auto"
+                 :width (u/px 900)}]
+   (let [files-top (u/px 280)]
+     [[:.section-container {:position "absolute"
+                            :top 0 :left 0
+                            :height files-top :width "100%"
+                            :background-color "#ddd"}]
+      [:.header-container {:position "absolute" :left 0 :top 0
+                           :width "100%" :height (u/px 30)
+                           :border {:style "solid" :width "0 0 1 0"}
+                           :font {:family ["Bitter" "sans-serif"]
+                                  :weight 400
+                                  :size (u/rem 0.7)}}]])
    [:.header-text
     [:h4 {:margin 0 :padding "0.5rem 0.5rem 0.5rem 0.5rem"}]]
    [:revision-description {:margin-left "2rem" :height "2.5rem"}]))
@@ -125,43 +141,39 @@
   (goog.style/setStyles @style-node styles)
   (reset! style-node (goog.style/installStyles styles)))
 
-(defn header []
-  [:div.header-section-container>div.header-section-container-2
-   
+(defn home-ui-header []
+  [:div.header-container
    [:div.header-text
-    [:div.lfloat {:style {:margin-right "0.5rem"}}
-     ;[:i.fa.fa-undo {:aria-hidden "true"}]
-     [:h4 "Back"]]
-    [:h4.lfloat "Settings"]
-    [:h4.rfloat
+    [:h4.lfloat.clickable "Settings"]
+    [:h4.rfloat.clickable
      @(p/q '[:find ?n .
              :where [?e]
              [?e :user/name ?n]]
            db-conn)]]])
 
-(def revisions
-  {:master {:description "[master] Branding v1"
-            :tags ["branding" "logo" "milestone"]
-            :files [{:file "logo.psd" :last-commit "[master] Branding v1"}
-                    {:file "file1.psd" :last-commit "Simplified logo; reduced number of colors"}
-                    {:file "file2.psd" :last-commit "[master] Branding v1"}
-                    {:file "file3.png" :last-commit "[master] Branding v1"}
-                    {:file "file4.jpg" :last-commit "[master] Branding v1"}
-                    {:file "file5.png" :last-commit "[master] Branding v1"}
-                    {:file "file6.jpg" :last-commit "[master] Branding v1"}]}
-   :head1 {:description "Simplified logo; reduced number of colors"
-           :tags ["logo" "tweak"]
-           :files [{:file "logo.psd" :last-commit "Simplified logo; reduced number of colors"}
-                   {:file "file1.psd" :last-commit "General structure for branding"}]}})
+(defn home-ui []
+  [:div.section-container
+   [:h1.clickable {:on-click #(reset! (:active-project ui-state) "Example")} "Hello"]
+   [home-ui-header]])
 
-(defonce ui-state
-  {:selected-revision (r/atom (:master revisions))})
+(defn project-ui-header []
+  [:div.header-container
+   [:div.header-text
+    [:div.lfloat {:style {:margin-right "0.5rem"}}
+     ;[:i.fa.fa-undo {:aria-hidden "true"}]
+     [:h4.clickable {:on-click #(reset! (:active-project ui-state) nil)} "Back"]]
+    [:h4.lfloat.clickable "Settings"]
+    [:h4.rfloat.clickable
+     @(p/q '[:find ?n .
+             :where [?e]
+             [?e :user/name ?n]]
+           db-conn)]]])
 
-(defn main []
+(defn project-ui []
   (let [hover-revision (r/atom false)]
     (fn []
-      [:div
-       [:div.graph-section-container>div.graph-section-container-2
+      [:div.section-container
+       [:div
         [:div.center
          [:object {:type "image/svg+xml" :data "svg/graph-prototype.svg"}
           "Your browser does not support SVG"]
@@ -170,33 +182,35 @@
            [:circle {:cx 699 :cy 186
                      :r 11 :style {:fill "#f7a032" :stroke "#666" :stroke-width "3"}
                      :cursor "pointer"
-                     :on-click #(reset! (:selected-revision ui-state) (:master revisions))}]
+                     :on-click #(reset! (:active-revision ui-state) (:master revisions))}]
            [:circle {:cx 646.5 :cy 186
                      :r 9 :style {:fill "#888"}
                      :cursor "pointer"
                      :on-mouse-over #(reset! hover-revision true)
                      :on-mouse-out #(reset! hover-revision false)
-                     :on-click #(reset! (:selected-revision ui-state) (:head1 revisions))}]
+                     :on-click #(reset! (:active-revision ui-state) (:head1 revisions))}]
            (when @hover-revision
              [:g
               [:rect {:x 485 :y 193 :width 200 :height 20 :fill "#fff"}]
               [:text {:x 660 :y 208 :font-family "Oswald" :font-size "0.8rem" :text-anchor "end"} "Simplified logo; reduced number of colors"]])]]]]
-       (let [selected-revision @(:selected-revision ui-state)]
-         [:div.files-section-container>div.files-section-container-2
-          [:h2.revision-description (:description selected-revision)]
-          [:h5.revision-description "Tags: " (clojure.string/join ", " (:tags selected-revision))]
+       (let [active-revision @(:active-revision ui-state)]
+         [:div
+          [:h2.revision-description (:description active-revision)]
+          [:h5.revision-description "Tags: " (clojure.string/join ", " (:tags active-revision))]
           [:div.files-listing
-           (for [{:keys [file last-commit]} (:files selected-revision)]
+           (for [{:keys [file last-commit]} (:files active-revision)]
              [:div.grid {:key file :style {:height "40px" :border-style "solid" :border-width "0 0 1 0" :border-color "#ccc"}}
               [:div.file-item.col-3 [:p.nomargin {:style {:line-height "40px" :height "40px" :color "#008cb7" :cursor "pointer"}} file]]
               [:div.file-item.col-9 [:p.nomargin {:style {:line-height "40px" :height "40px"}} last-commit]]])]])
-       [header]])))
+       [project-ui-header]])))
 
 (defn app []
   (let [canvas-dom (atom nil)
         monet-canvas (atom nil)]
     [:div.container
-     [main]]))
+     (if @(:active-project ui-state)
+       [project-ui]
+       [home-ui])]))
 
 ;;
 ;; Init
