4c7c54a7f123747554499e4e1aee1cfccf8a2633
alvatar <a@fourthbit.com>
2017-09-13 11:03:38 -0500

    Add annotations textarea

--- src/cljs/z03/core.cljs
+++ src/cljs/z03/core.cljs
@@ -42,8 +42,8 @@
 ;;
 
 (def projects
-  [{:id "google-animations"
-    :description "Showcase Google animations"
+  [{:id "ios7-templates"
+    :description "iOS7 Template System"
     :commits ["[master] First version"]}
    {:id "rick-interstellar-enterprises-branding"
     :description "Rick Interstellar Enterprises: branding"
@@ -133,6 +133,11 @@
               :line-height (u/rem 1.5)
               :border {:style "solid" :width "0 0 1 0"}}]
    [:h5 {:line-height (u/rem 1.6)}]
+   [:textarea
+    {:font-family ["Oswald" "sans-serif"]
+     :font-weight 200
+     :border {:style "solid" :width "0 0 0 2"}
+     :background-color "#eee"}]
    [:.fill-parent {:width "100%" :height "100%"}]
    [:.lfloat {:float "left"}]
    [:.rfloat {:float "right"}]
@@ -142,6 +147,7 @@
    [:.divider {:border {:style "solid" :width "0 0 1 0"}}]
    ;; Colors
    [:.bg-aqua {:background-color "#7fdbff"}]
+   [:.aqua {:color "#7fdbff"}]
    ;; Animations
    drawer-animation-show
    drawer-animation-hide
@@ -269,26 +275,42 @@
 
 (defonce file-annotations (r/atom []))
 
+(defn click-on-annotation [_x _y]
+  (let [r 13]
+   (some (fn [{:keys [x y id] :as e}]
+           (and (< _x (+ x r))
+                (> _x (- x r))
+                (< _y (+ y r))
+                (> _y (- y r))
+                e))
+         @file-annotations)))
+
 (def editable
   (let [image-scale (r/atom 1)
         image-x (r/atom 0)
         image-y (r/atom 0)
         space-down (r/atom false)
         key-down-listener #(when (= (.-keyCode %) goog.events.KeyCodes.SPACE) (reset! space-down true))
-        key-up-listener #(when (= (.-keyCode %) goog.events.KeyCodes.SPACE) (reset! space-down false))]
+        key-up-listener #(when (= (.-keyCode %) goog.events.KeyCodes.SPACE) (reset! space-down false))
+        annotation-edit (r/atom nil)]
     (with-meta
       (fn []
-        [:div {:style {:position "relative" :width "100%" :height "100%"}}
-         [:div {:style {:position "absolute" :top 0 :left 0}}
-          [:img {:style {:transform (gstring/format "scale(%s)" @image-scale)}
-                 :src "img/google_motion_system.gif"}]
-          [:div {:style {:position "absolute" :top 0 :left 0 :width "100%" :height "100%"}}
-           [:svg {:width "100%" :height "100%"
-                  :style {:transform (gstring/format "scale(%s)" @image-scale)}}
-            (for [{:keys [id x y]} @file-annotations]
-              [:circle {:cx x :cy y
-                        :r 10 :style {:fill "#f7a032" :stroke "rgba(255, 124, 43, 0.3)" :stroke-width "3px"}
-                        :cursor "pointer"}])]]]])
+        [:div {:style {:position "absolute" :top 0 :left 0}}
+         [:img {:style {:transform (gstring/format "scale(%s)" @image-scale)}
+                :src "img/template_ios7.png"}]
+         [:div {:style {:position "absolute" :top 0 :left 0 :width "100%" :height "100%"}}
+          [:svg {:width "100%" :height "100%"
+                 :style {:transform (gstring/format "scale(%s)" @image-scale)}}
+           (for [{:keys [id x y]} @file-annotations]
+             [:circle {:cx x :cy y
+                       :r 10 :style {:fill "#f7a032" :stroke "rgba(255, 124, 43, 0.3)" :stroke-width "3px"}
+                       :cursor "pointer"}])]
+          (when-let [{:keys [x y]} @annotation-edit]
+            [:div {:style {:position "absolute" :left x :top y}}
+             [:textarea {:rows 4 :cols 50}]
+             [:div
+              [:h4.rfloat.link {:style {:margin-top 0} :on-click #(reset! annotation-edit nil)} "Save"]
+              [:h4.rfloat.link {:style {:margin "0 10px 0 0"} :on-click #(reset! annotation-edit nil)} "Cancel"]]])]])
       {:component-did-mount
        (fn [this]
          (let [node (r/dom-node this)]
@@ -300,23 +322,25 @@
            (goog.events/listen node
                                goog.events.EventType/MOUSEDOWN
                                (fn [e]
-                                 (if @space-down
-                                   (let [drag (fx/Dragger. node)
-                                         local-state (r/state this)]
-                                     (.setLimits drag (goog.math.Rect. 0 0 400 400))
-                                     #_(.addEventListener drag
-                                                          goog.fx.Dragger.EventType/DRAG
-                                                          (fn [d]
-                                                              (let [dx (-> d .-dragger .-deltaX)
-                                                                    dy (-> d .-dragger .-deltaY)]
-                                                                (log* (str "x: " dx " y: " dy))
-                                                                (swap! image-x (fn [x] (gmath/clamp dx 0 x)))
-                                                                (swap! image-y (fn [y] (gmath/clamp dy 0 y))))))
-                                     (.addEventListener drag goog.fx.Dragger.EventType/END #(.dispose drag))
-                                     (.startDrag drag e))
-                                   (swap! file-annotations conj {:x (.-offsetX e)
-                                                                 :y (.-offsetY e)
-                                                                 :id (rand-int 99999999)}))))))
+                                 (let [x (.-offsetX e) y (.-offsetY e)]
+                                   (cond @space-down
+                                         (let [drag (fx/Dragger. node)
+                                               local-state (r/state this)]
+                                           (.setLimits drag (goog.math.Rect. 0 0 400 400))
+                                           #_(.addEventListener drag
+                                                                goog.fx.Dragger.EventType/DRAG
+                                                                (fn [d]
+                                                                  (let [dx (-> d .-dragger .-deltaX)
+                                                                        dy (-> d .-dragger .-deltaY)]
+                                                                    (log* (str "x: " dx " y: " dy))
+                                                                    (swap! image-x (fn [x] (gmath/clamp dx 0 x)))
+                                                                    (swap! image-y (fn [y] (gmath/clamp dy 0 y))))))
+                                           (.addEventListener drag goog.fx.Dragger.EventType/END #(.dispose drag))
+                                           (.startDrag drag e))
+                                         (not @annotation-edit)
+                                         (if-let [ann (click-on-annotation x y)]
+                                           (reset! annotation-edit {:x (+ 7 (:x ann)) :y (+ 7 (:y ann))})
+                                           (swap! file-annotations conj {:x x :y y :id (rand-int 99999999)}))))))))
        :component-will-unmount
        (fn [this]
          (goog.events/unlisten js/document goog.events.EventType.KEYUP key-up-listener)
