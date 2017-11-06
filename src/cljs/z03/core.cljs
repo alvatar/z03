7bdab69c5105eddff351a0c7675e5e8200bafbb9
alvatar <a@fourthbit.com>
2017-11-06 11:54:59 -0500

    Some aesthetics back to life

--- src/cljs/z03/core.cljs
+++ src/cljs/z03/core.cljs
@@ -159,64 +159,6 @@
              [?e :user/name ?n]]
            db-conn)]]])
 
-(defc project-ui
-  < r/reactive
-  []
-  (let [files (react (:files app-state))
-        active-commit (react (:active-commit app-state))
-        current-path (not-empty (react (:current-path app-state)))
-        entries (mapv (fn [[k v]]
-                        ;; this element in the structure is a string in the case of folders
-                        (if (string? (first (first v)))
-                          {:filename k :filetype "directory"}
-                          {:filename k :filetype "file"
-                           :subject (:subject v) :age (:age v)}))
-                      (if current-path (get-in files current-path) files))]
-    [:div {:style {:padding-top "80px" :height "220px"}}
-     [:div#graph-container [:canvas#gitGraph]]
-     (when-let [hover-commit (react (:hover-commit app-state))]
-       [:div#commit-head
-        [:div.grid-noGutter
-         [:div.col-6
-          [:h5 (:subject hover-commit)]
-          [:h5 (gstring/format "%s (%s)" (:age hover-commit) (:author hover-commit))]]
-         [:div.col-6
-          [:input.flat-button {:type "submit" :value "add version"}]
-          [:input.flat-button {:type "submit" :value "add commit"}]]]])
-     (if-not (react (:commits app-state))
-       [:div {:style {:margin-top "-100px"}}
-        [:div.center-aligner
-         [:div.spinner [:div.double-bounce1] [:div.double-bounce2]]]]
-       [:div
-        [:div.grid.files-listing-header
-         [:div.col-9>h5.author [:strong (:author active-commit)] " " (:subject active-commit)]
-         [:div.col-3>h5.rfloat.link "get presentation link"]]
-        [:div.files-listing
-         (let [grouped (group-by #(= (:filetype %) "directory") entries)
-               directories (get grouped true)
-               files (get grouped false)]
-           (concat
-            (when current-path
-              [[:div.grid {:key "dir-up" :style {:height "40px" :border-style "solid" :border-width "0 0 1 0" :border-color "#ccc"}}
-                [:div.file-item.col-3.clickable {:on-click #(swap! (:current-path app-state) butlast)}
-                 [:p.nomargin {:style {:line-height "40px" :height "40px" :color "#025382"}} ".."]]]])
-            (for [{:keys [filename filetype age subject]} (sort-by :filename directories)]
-              [:div.grid {:key filename :style {:height "40px" :border-style "solid" :border-width "0 0 1 0" :border-color "#ccc"}}
-               [:div.file-item.col-3.clickable {:on-click #(swap! (:current-path app-state) concat [filename])}
-                [:i.fa.fa-folder.file-icon {:aria-hidden "true"}]
-                [:p.nomargin {:style {:line-height "40px" :height "40px" :color "#025382"}} (str filename "/")]]
-               [:div.file-item.col-7 [:p.nomargin {:style {:line-height "40px" :height "40px"}} subject]]
-               [:div.file-item.col-2 [:p.nomargin {:style {:line-height "40px" :height "40px"}} age]]])
-            (for [{:keys [filename filetype age subject]} (sort-by :filename files)]
-              [:div.grid {:key filename :style {:height "40px" :border-style "solid" :border-width "0 0 1 0" :border-color "#ccc"}}
-               [:div.file-item.col-3.clickable {:on-click #(reset! (:active-file app-state) filename)}
-                [:i.fa.fa-file.file-icon {:aria-hidden "true"}]
-                [:p.nomargin {:style {:line-height "40px" :height "40px" :color "#008cb7"}} filename]]
-               [:div.file-item.col-7 [:p.nomargin {:style {:line-height "40px" :height "40px"}} subject]]
-               [:div.file-item.col-2 [:p.nomargin {:style {:line-height "40px" :height "40px"}} age]]])))]])
-     (project-ui-header)
-     (footer)]))
-
 (defn draw-git-graph []
   (when-let [commits @(:commits app-state)]
     (let [template (js/GitGraph.Template.
@@ -233,8 +175,6 @@
                                 "mode" "compact"})
           fork-points @(:fork-points app-state)
           branches (atom {})]
-      (let [graph-div (js/document.getElementById "graph-container")]
-        )
       (doto (oget gg "canvas")
         (.addEventListener "commit:mouseover" (fn [ev]
                                                 (reset! (:hover-commit app-state)
@@ -271,11 +211,69 @@
                   (do (.commit gg-branch commit-data)
                       (swap! branches assoc-in [branch-name :head] hash))))
             2 (js/alert "TODO: merge")
-            (js/alert "Octopus merges not supported. What are you doing?")))))))
+            (js/alert "Octopus merges not supported. What are you doing?"))))
+      ;; Scroll to end
+      (let [graph-div (js/document.getElementById "graph-container")]
+        (oset! graph-div "scrollLeft" (oget graph-div "scrollWidth"))))))
 
-#_(def project-ui
-  (with-meta project-ui*
-    {:component-did-mount draw-git-graph}))
+(defc project-ui
+  < r/reactive
+  []
+  (let [files (react (:files app-state))
+        active-commit (react (:active-commit app-state))
+        current-path (not-empty (react (:current-path app-state)))
+        entries (mapv (fn [[k v]]
+                        ;; this element in the structure is a string in the case of folders
+                        (if (string? (first (first v)))
+                          {:filename k :filetype "directory"}
+                          {:filename k :filetype "file"
+                           :subject (:subject v) :age (:age v)}))
+                      (if current-path (get-in files current-path) files))]
+    [:div {:style {:padding-top "80px" :height "220px"}}
+     [:div#graph-container [:canvas#gitGraph]]
+     (when-let [hover-commit (react (:hover-commit app-state))]
+       [:div#commit-head
+        [:div.grid-noGutter
+         [:div.col-6
+          [:h5 (:subject hover-commit)]
+          [:h5 (gstring/format "%s (%s)" (:age hover-commit) (:author hover-commit))]]
+         [:div.col-6
+          [:input.flat-button {:type "submit" :value "add version"}]
+          [:input.flat-button {:type "submit" :value "add commit"}]]]])
+     (if-not (react (:commits app-state))
+       [:div
+        [:div.center-aligner
+         [:div.spinner [:div.double-bounce1] [:div.double-bounce2]]]]
+       [:div
+        [:div.grid-noGutter.files-listing-header
+         [:div.col-9 [:h5.author
+                      [:strong (:author active-commit)] " " (:subject active-commit)]]
+         [:div.col-3 [:h5.rfloat.link "get presentation link"]]]
+        [:div.files-listing
+         (let [grouped (group-by #(= (:filetype %) "directory") entries)
+               directories (get grouped true)
+               files (get grouped false)]
+           (concat
+            (when current-path
+              [[:div.grid-noGutter {:key "dir-up" :style {:height "40px" :border-style "solid" :border-width "0 0 1 0" :border-color "#ccc"}}
+                [:div.file-item.col-3.clickable {:on-click #(swap! (:current-path app-state) butlast)}
+                 [:p.nomargin {:style {:line-height "40px" :height "40px" :color "#025382"}} ".."]]]])
+            (for [{:keys [filename filetype age subject]} (sort-by :filename directories)]
+              [:div.grid-noGutter {:key filename :style {:height "40px" :border-style "solid" :border-width "0 0 1 0" :border-color "#ccc"}}
+               [:div.file-item.col-3.clickable {:on-click #(swap! (:current-path app-state) concat [filename])}
+                [:i.fa.fa-folder.file-icon {:aria-hidden "true"}]
+                [:p.nomargin {:style {:line-height "40px" :height "40px" :color "#025382"}} (str filename "/")]]
+               [:div.file-item.col-7 [:p.nomargin {:style {:line-height "40px" :height "40px"}} subject]]
+               [:div.file-item.col-2 [:p.nomargin {:style {:line-height "40px" :height "40px"}} age]]])
+            (for [{:keys [filename filetype age subject]} (sort-by :filename files)]
+              [:div.grid-noGutter {:key filename :style {:height "40px" :border-style "solid" :border-width "0 0 1 0" :border-color "#ccc"}}
+               [:div.file-item.col-3.clickable {:on-click #(reset! (:active-file app-state) filename)}
+                [:i.fa.fa-file.file-icon {:aria-hidden "true"}]
+                [:p.nomargin {:style {:line-height "40px" :height "40px" :color "#008cb7"}} filename]]
+               [:div.file-item.col-7 [:p.nomargin {:style {:line-height "40px" :height "40px"}} subject]]
+               [:div.file-item.col-2 [:p.nomargin {:style {:line-height "40px" :height "40px"}} age]]])))]])
+     (project-ui-header)
+     (footer)]))
 
 (defc app
   < r/reactive
@@ -295,4 +293,4 @@
 
 (r/mount (app) (js/document.getElementById "app"))
 
-(client/start-router!)
+(defonce _router (client/start-router!))
